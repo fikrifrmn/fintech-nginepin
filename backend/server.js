87dcf9008826b290750077b7a78aa658edc5b680
Fikri <140414207+fikrifrmn@users.noreply.github.com>
2026-01-11 12:47:06 +0700

    fix redeploy

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "http://127.0.0.1:5500",
+  origin: "https://fintech-nginepin.vercel.app",
   credentials: true,
 }));
 app.use(bodyParser.json());
